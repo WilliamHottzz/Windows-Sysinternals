d72ed78edb6bdcb2e3472ecdbd0dc6d6b7023e95
WilliamHottzz <56490828+WilliamHottzz@users.noreply.github.com>
2022-01-26 14:52:26 -0600

    Add files via upload

---
+++ Morningstar/Core/Foundation/Message/Error/FailedProcessOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morningstar.Core.Foundation.Message.Error
+{
+    internal class FailedProcessOperations
+    {
+        public string FailedStartingProcess = "Failed to start process!";
+        public string FailedLoadingProcessModules = "Failed reloading process modules!";
+        public string FailedProcessIdNotFound = "Process ID does not exist!";
+        public string FailedTerminatingProcess = "Could not terminate process, is it still running?";
+        public string FailedProcessNamesDoNotMatch = "Process names do not match! can NOT terminate";
+    }
+}
