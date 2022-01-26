699c709b1884d3ad515656b19a112e358b194c1c
WilliamHottzz <56490828+WilliamHottzz@users.noreply.github.com>
2022-01-26 14:54:45 -0600

    Add files via upload

---
+++ Morningstar/Core/Foundation/ProcessOperations/Performance/Resources.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morningstar.Core.Foundation.ProcessOperations.Performance
+{
+    internal class Resources
+    {
+        public string ProcessorUsage { get; set; }
+        public string MemoryUsage { get; set; }
+        public string Handles { get; set; }
+        public string Threads { get; set; }
+    }
+}
